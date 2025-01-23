60ed4f56864ec8741b5210e0907ffe40fae8e1d4
RedMulaCrackIn <giony2003@gmail.com>
2025-01-23 11:25:41 +0100

    Load test set and pre-trained model for evaluation

--- NN/RS/finaltest.py
+++ NN/RS/finaltest.py
@@ -3,3 +3,12 @@ import pandas as pd
 from sklearn.metrics import classification_report
 import numpy as np
 
+# Caricamento del test set
+ts = pd.read_csv("test_set.csv", index_col=False)
+
+# Separazione delle feature (X) e del target (y)
+ts_x = ts.drop('result', axis=1)
+ts_y = ts['result']
+
+# Caricamento del modello pre-addestrato
+best_model = load_model('best_model.h5')
