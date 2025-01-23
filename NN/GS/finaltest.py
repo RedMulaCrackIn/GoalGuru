8286f6dc478625b807c4c46b8202540f0343593a
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-23 11:01:17 +0100

    caricamento modello addestrato

--- NN/GS/finaltest.py
+++ NN/GS/finaltest.py
@@ -5,3 +5,14 @@ import numpy as np
 
 # Caricamento del test set
 ts = pd.read_csv("test_set.csv", index_col=False)
+
+# Separazione delle feature (X) e del target (y)
+ts_x = ts.drop('result', axis=1)
+ts_y = ts['result']
+
+# Caricamento del modello pre-addestrato
+best_model = load_model('best_model.h5')
+
+# Visualizzazione del riepilogo del modello
+print("Riepilogo del modello:")
+print(best_model.summary())
