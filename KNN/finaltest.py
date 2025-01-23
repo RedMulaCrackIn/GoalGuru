b55a9fc32039cf547285ac85a5e0c19c1e8a603a
RedMulaCrackIn <giony2003@gmail.com>
2025-01-23 10:54:21 +0100

    Add initial implementation of KNN final test script with necessary imports

---
+++ KNN/finaltest.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+import numpy as np
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+
