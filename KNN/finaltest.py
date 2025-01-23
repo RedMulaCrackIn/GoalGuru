bf6b920d7bdbd527cb5927734817f940e8466e85
RedMulaCrackIn <giony2003@gmail.com>
2025-01-23 10:54:35 +0100

    Add test set loading and KNN model deserialization to final test script

--- KNN/finaltest.py
+++ KNN/finaltest.py
@@ -5,3 +5,13 @@ import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Caricamento del test set
+ts = pd.read_csv("test_set.csv", index_col=False)
+
+# Separazione delle feature (X) e del target (y)
+ts_x = ts.drop('result', axis=1)
+ts_y = ts['result']
+
+# Caricamento del modello KNN pre-addestrato
+with open("best_knn_model.pkl", "rb") as f:
+    best_model = pickle.load(f)
