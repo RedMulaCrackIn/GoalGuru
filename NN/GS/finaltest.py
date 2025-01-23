55203fb80787e470a271f0b0430723d0f905b8b7
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-23 11:01:57 +0100

    Aggiunta della valutazione del modello e stampa dei risultati sul test set

--- NN/GS/finaltest.py
+++ NN/GS/finaltest.py
@@ -16,3 +16,15 @@ best_model = load_model('best_model.h5')
 # Visualizzazione del riepilogo del modello
 print("Riepilogo del modello:")
 print(best_model.summary())
+
+
+# Valutazione del modello sul test set
+test_loss, test_accuracy = best_model.evaluate(ts_x, ts_y, verbose=1)
+
+# Stampa della loss e dell'accuratezza
+print(f"Test loss: {test_loss}")
+print(f"Test accuracy: {test_accuracy}")
+
+# Predizione delle classi sul test set
+y_pred = best_model.predict(ts_x)
+y_pred_classes = np.argmax(y_pred, axis=1)
